eaa59b1f939d13fadafce5b2dd9bbce45f2c9a8f
rafaath <86877018+rafaath@users.noreply.github.com>
2025-06-11 03:12:50 +0530

    Delete nomnom directory

--- nomnom/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 py-8 mt-16" id="contact">
-      <div className="max-w-7xl mx-auto px-4 text-center text-sm text-gray-600">
-        © {new Date().getFullYear()} NomNom. All rights reserved.
-      </div>
-    </footer>
-  );
-}
